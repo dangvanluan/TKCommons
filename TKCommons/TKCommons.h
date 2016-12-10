14bc9b29cd5f3d703ecfba55adb4975652a67cbc
dangvanluan <dangvanluan.vn@gmail.com>
2016-12-11 04:06:55 +0700

    Add files via upload

---
+++ TKCommons/TKCommons.h
@@ -0,0 +1,19 @@
+//
+//  TKCommons.h
+//  TKCommons
+//
+//  Created by Dang Van Luan on 12/11/16.
+//  Copyright © 2016 Dang Van Luan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKCommons.
+FOUNDATION_EXPORT double TKCommonsVersionNumber;
+
+//! Project version string for TKCommons.
+FOUNDATION_EXPORT const unsigned char TKCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKCommons/PublicHeader.h>
+
+
